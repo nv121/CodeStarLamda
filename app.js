306653bf31a35a138d352ee6a830fd80957f292c
“CEOho-at-994344697834” <noe121@uab.edu>
2021-04-20 22:34:39 -0500

    Changes on app.js added string variables

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World! DogeCoin Bro"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World! DogeCoin to the Moon"
   });
 });
 
